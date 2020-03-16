ddf26042ae0a2bcd659539f0c69467b55ac598fe
Stoffle <stoffle@users.noreply.github.com>
2020-03-16 14:00:38 +0000

    Updated version number and URL

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setup(
     name="BayNet",
-    version="0.1.0",
+    version="0.1.1",
     author="Chris Robinson",
     author_email="c.f.robinson@sussex.ac.uk",
     description="(another) Python Bayesian Network library",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Stoffle/BayNet",
+    url="https://github.com/predictive-analytics-lab/BayNet",
     packages=find_packages(exclude=("tests",)),
     python_requires=">=3.7",
     install_requires=["python-igraph < 0.8.0", "numpy >= 1.17.2", "pandas >= 0.25",],
