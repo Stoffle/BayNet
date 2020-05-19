61f222b9ba1bdb21d18c968e4077945479d46897
Chris Robinson <stoffle@users.noreply.github.com>
2020-05-19 12:18:36 +0100

    made inferred levels properly cast to str

--- run_pylint.py
+++ run_pylint.py
@@ -5,7 +5,7 @@ To stop pylint breaking the build if <85% perfect
 import sys
 from pylint.lint import Run
 
-RESULTS = Run(['./baynet/'], do_exit=False)
+RESULTS = Run(['./baynet/'], exit=False)
 SCORE = RESULTS.linter.stats['global_note']
 print("Your code has been rated at {:.2f}/10".format(SCORE))
 
