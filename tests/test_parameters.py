49500c857cab4446da03f159114c3ba276d26bde
Bradley-Butcher <bradleydpbutcher@gmail.com>
2020-05-19 15:40:31 +0100

    added test for defaults

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -61,6 +61,15 @@ def test_dfe_parameters():
     assert np.array_equal(dag.vs[0]['CPD'].cumsum_array, np.array([0.55, 1]))
 
 
+def test_dfe_default_parameters():
+    dag = DAG.from_modelstring('[A]')
+    dag.vs['levels'] = [["A", "B"] for v in dag.vs]
+    data = pd.DataFrame({'A': [0]})
+    dag.estimate_parameters(data, method="dfe", method_args={"iterations": -1, "learning_rate": 0.1})
+    # x + (x * 0.1) assert checks if data was changed 4 times (len * 4, as described in the paper)
+    assert np.array_equal(dag.vs[0]['CPD'].cumsum_array, np.array([0.67195, 1]))
+
+
 def test_CPT_estimate_other(test_dag):
     test_dag.vs['levels'] = [["A", "B"] for v in test_dag.vs]
     with pytest.raises(NotImplementedError):
